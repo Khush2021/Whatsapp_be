3428ea49644da0da6bad52f73e1c021e4775889e
work421 <yospam4@gmail.com>
2023-11-08 09:55:18 +0530

    conversation and message

---
+++ src/controllers/conversation.controller.js
@@ -0,0 +1,51 @@
+import createHttpError from "http-errors";
+import logger from "../configs/logger.config.js";
+import {
+  doesConversationExist,
+  createConversation,
+  getUserConversations,
+} from "../services/conversation.service.js";
+import { findUser } from "../services/user.service.js";
+
+export const create_open_conversation = async (req, res, next) => {
+  try {
+    const sender_id = req.user.userId;
+    const { receiver_id } = req.body;
+    //check if receiver_id provided
+    if (!receiver_id) {
+      logger.error(
+        "please provide the userId you wanna start a conversation with!"
+      );
+      throw createHttpError.BadRequest("Something went wrong!");
+    }
+    //check if chat exists
+    const existed_conversation = await doesConversationExist(
+      sender_id,
+      receiver_id
+    );
+    if (existed_conversation) {
+      res.json(existed_conversation);
+    } else {
+      let receiver_user = await findUser(receiver_id);
+      let convoData = {
+        name: receiver_user.name,
+        isGroup: false,
+        users: [sender_id, receiver_id],
+      };
+      const newConvo = await createConversation(convoData);
+      res.json(newConvo);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getConversation = async (req, res, next) => {
+  try {
+    const user_id = req.user.userId;
+    const conversations = await getUserConversations(user_id);
+    res.json(conversations);
+  } catch (error) {
+    next(error);
+  }
+};
